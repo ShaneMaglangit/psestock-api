5656ed70ed3b0cb672b83551450296a8d0377ae0
ShaneMaglangit <shanedmaglangit@gmail.com>
2021-04-09 13:02:58 +0800

    Add GET request for stock data by code

--- main/main.go
+++ main/main.go
@@ -1,52 +1,77 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
 )
 
 type Stock struct {
-	Code  string
-	Name  string
-	Price float32
+	Name     string  `json:"name"`
+	Code     string  `json:"code"`
+	Date     string  `json:"date"`
+	Price    float32 `json:"price"`
+	Open     float32 `json:"open"`
+	High     float32 `json:"high"`
+	Low      float32 `json:"low"`
+	Volume   string  `json:"volume"`
+	Change   string  `json:"change"`
+	Currency string  `json:"currency"`
 }
 
-var stocks []Stock
+var stocks = make([]Stock, 0)
 
 func main() {
 	handle()
 }
 
 func init() {
-	stocks = []Stock{
-		{"A", "AlphabetA", 1.25},
-		{"B", "AlphabetB", 1.5},
-		{"C", "AlphabetC", 2.0},
+	file, err := os.Open("main/stocks.csv")
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	data, err := csv.NewReader(file).ReadAll()
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+
+	for _, entry := range data {
+		price, _ := strconv.ParseFloat(entry[3], 32)
+		open, _ := strconv.ParseFloat(entry[4], 32)
+		high, _ := strconv.ParseFloat(entry[5], 32)
+		low, _ := strconv.ParseFloat(entry[6], 32)
+
+		stock := Stock{
+			Name:     entry[0],
+			Code:     entry[1],
+			Date:     entry[2],
+			Price:    float32(price),
+			Open:     float32(open),
+			High:     float32(high),
+			Low:      float32(low),
+			Volume:   entry[7],
+			Change:   entry[8],
+			Currency: "PHP",
+		}
+
+		stocks = append(stocks, stock)
 	}
 }
 
 func handle() {
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/", defaultHandler)
-	router.HandleFunc("/stocks", getAllStocks)
 	router.HandleFunc("/stocks/{code}", getStock)
 	log.Println("Started server at 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
-}
-
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<h1>Hello World</h1>")
-}
-
-func getAllStocks(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint hit: getAllStocks")
-
-	// Encode stocks and write as part of response
-	_ = json.NewEncoder(w).Encode(stocks)
 }
 
 func getStock(w http.ResponseWriter, r *http.Request) {
