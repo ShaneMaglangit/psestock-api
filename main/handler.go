871318cc86425facddac153ce12e65795362095d
ShaneMaglangit <shanedmaglangit@gmail.com>
2021-04-11 17:25:11 +0800

    Create struct for request parameters and added sorting option

--- main/handler.go
+++ main/handler.go
@@ -10,40 +10,60 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 )
+
+type Stock struct {
+	Code   string    `json:"code" bson:"code"`
+	Date   time.Time `json:"date" bson:"date"`
+	Price  float32   `json:"price" bson:"price"`
+	Open   float32   `json:"open" bson:"open"`
+	High   float32   `json:"high" bson:"high"`
+	Low    float32   `json:"low" bson:"low"`
+	Volume string    `json:"volume" bson:"volume"`
+	Change string    `json:"change" bson:"change"`
+}
+
+type RequestParams struct {
+	code      string
+	startDate time.Time
+	endDate   time.Time
+	asc       bool
+	limit     int64
+}
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint hit: getStock")
 
 	// Get the params from the request
-	params := mux.Vars(r)
-	code := strings.ToUpper(params["code"])
+	vars := mux.Vars(r)
 
-	// Time params
-	start := r.URL.Query().Get("start")
-	end := r.URL.Query().Get("end")
-
-	// Count params
-	limit, _ := strconv.ParseInt(r.URL.Query().Get("limit"), 10, 8)
+	// Extract the params
+	params := RequestParams{
+		code:      strings.ToUpper(vars["code"]),
+		startDate: parseDate(time.Date(1927, time.August, 8, 0, 0, 0, 0, time.UTC), r.URL.Query().Get("start")),
+		endDate:   parseDate(time.Now(), r.URL.Query().Get("end")),
+		asc:       strings.ToUpper(r.URL.Query().Get("asc")) == "TRUE",
+		limit:     getLimit(r.URL.Query().Get("limit")),
+	}
 
 	// Find the stock with the matching code
-	stocks := queryStockFromDb(code, start, end, limit)
+	stocks := queryStockFromDb(params)
 
 	// Write data to response
 	_ = json.NewEncoder(w).Encode(stocks)
 }
 
-func queryStockFromDb(code, start, end string, limit int64) []Stock {
+func queryStockFromDb(params RequestParams) []Stock {
 	stocks := make([]Stock, 0)
-	startDate, endDate := parseStartEndDate(start, end)
 
 	db := mongoClient.Database("psestocks")
 	collection := db.Collection("prices")
 
 	findOptions := options.Find()
-	findOptions.SetLimit(limit)
+	findOptions.SetLimit(params.limit)
 
-	cursor, err := collection.Find(ctx, bson.M{"code": code, "date": bson.M{"$gte": startDate, "$lte": endDate}}, findOptions)
+	cursor, err := collection.Find(ctx, bson.M{"code": params.code, "date": bson.M{"$gte": params.startDate, "$lte": params.endDate}}, findOptions)
 	if err != nil {
 		log.Fatal(err)
 		return nil
@@ -56,8 +76,33 @@ func queryStockFromDb(code, start, end string, limit int64) []Stock {
 			log.Fatal(err)
 			return nil
 		}
+
+		if params.asc {
+			stocks = append([]Stock{stock}, stocks...)
+			continue
+		}
 		stocks = append(stocks, stock)
 	}
 
 	return stocks
+}
+
+func parseDate(def time.Time, timeString string) time.Time {
+	if timeString == "" {
+		return def
+	}
+	date, _ := time.Parse("2006-01-02", timeString)
+	return date
+}
+
+func getLimit(s string) int64 {
+	switch s {
+	case "":
+		return 1
+	case "all":
+		return 0
+	default:
+		limit, _ := strconv.ParseInt(s, 10, 8)
+		return limit
+	}
 }
